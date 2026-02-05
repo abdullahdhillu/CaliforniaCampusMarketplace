dedfaa05f0db75e1891fbbfc79ddf654213dfc84
Muhammad <abdullahdhillu99@gmail.com>
2026-02-05 11:40:04 -0800

    minor bug fix

--- server/src/controllers/productControllers.js
+++ server/src/controllers/productControllers.js
@@ -12,25 +12,21 @@ export async function getProducts(req, res, next) {
   const campus = await campusModel.findOne({slug});
   if (!campus) return next(createHttpError(404, 'campus not found'));
   const filter = {campusID: campus._id, status: 'Active'};
-  if (q) {
-    filter.$text = {$search: q};
-    const items =
-        await productModel.find(filter).sort({score: {$meta: 'textScore'}});
-    res.json({products: items.length, items})
-  }
+  if (q) filter.$text = {$search: q};
   if (category) filter.category = category;
   if (min || max) {
     filter.price = {};
     if (min) filter.price.$gte = Number(min);
     if (max) filter.price.$lte = Number(max);
   }
+  const sort = q? {score : {$meta: 'textScore'}} : {createdAt: -1}
   const items = await productModel.find(filter)
                     .sort({createdAt: -1})
                     .skip((Number(page) - 1) * Number(limit))
                     .limit(Number(limit))
                     .lean();
   console.log('Fetched Products');
-  res.json({items, page: Number(page), hasMore: items.length === Number(limit)})
+  return res.json({items, page: Number(page), hasMore: items.length === Number(limit)})
 }
 
 export async function createProduct(req, res, next) {
